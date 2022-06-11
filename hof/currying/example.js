f75d28d738c48c4b187426cfc26e9efa8bf62d7f
dev-bestow <75873160+Dev-BestOW@users.noreply.github.com>
2022-06-11 17:17:39 +0900

    :sparkles: 커링 arrow example

--- hof/currying/example.js
+++ hof/currying/example.js
@@ -8,5 +8,13 @@ function add (first) {
   }
 }
 
+const addByArrow = (first) => (second) => {
+  return first + second
+}
+
 const add10 = add(10);
 add10(1) // 11
+add(10)(1) // 11
+addByArrow(10)(1) // 11
+
+
